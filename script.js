581c11bc337f74e9b0731ac4fe2447012dca9aa5
Gabriel <gabrielcsilvasg@gmail.com>
2024-03-31 21:31:51 -0300

    "Adicionar conteúdos das tarefas ao HTML e limpar os campos de entrada"

--- script.js
+++ script.js
@@ -27,5 +27,47 @@ cancelBtn.addEventListener("click", () => {
 
 discardBtn.addEventListener("click", () => {
   confirmCloseDialog.close();
-  taskForm.classList.toggle("hidden");
+  reset();
 });
+
+const reset = () => {
+  titleInput.value = "";
+  dateInput.value = "";
+  descriptionInput.value = "";
+  currentTask = {};
+  taskForm.classList.toggle("hidden");
+};
+
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const dataArrIndex = taskData.findIndex((item) => {
+    item.id === currentTask.id;
+  });
+
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
+  };
+
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+
+  taskData.forEach(({ id, title, date, description }) => {
+    tasksContainer.innerHTML += `
+    <div class="task" id="${id}">
+    <p><strong>Title:</strong> ${title}</p>
+    <p><strong>Date:</strong> ${date}</p>
+    <p><strong>Description:</strong> ${description}</p>
+    <button type="button" class="btn">Edit</button>
+    <button type="button" class="btn">Delete</button>
+    </div>
+    `;
+reset();
+
+  });
+});
+
+console.log(taskData);
